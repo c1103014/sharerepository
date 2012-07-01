42493fba12794bb9d7b7f23308871e8ca9d00c8c
tomotomo <nakagawa20130907@yahoo.co.jp>
2012-07-01 13:42:12 +0900

    nakagawa

--- ServerOperation/ServerOperation/ServerOperation.cpp
+++ ServerOperation/ServerOperation/ServerOperation.cpp
@@ -11,19 +11,19 @@ void resize(int w, int h);
 void timer(int value);
 Renderer g_renderer;
 
-int main(int argc, char *argv[])
-{
-	//test
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutCreateWindow(argv[0]);
-	glutDisplayFunc(display);
-	glutReshapeFunc(resize);
-	glutTimerFunc(1000 / 30, timer, 0);
-	g_renderer.init();
-	glutMainLoop();
-	return 0;
-}
+//int main(int argc, char *argv[])
+//{
+//	//test
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+//	glutCreateWindow(argv[0]);
+//	glutDisplayFunc(display);
+//	glutReshapeFunc(resize);
+//	glutTimerFunc(1000 / 30, timer, 0);
+//	g_renderer.init();
+//	glutMainLoop();
+//	return 0;
+//}
 
 void display(void)
 {
